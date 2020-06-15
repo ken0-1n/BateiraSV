a61bff28f5b54f3e4f1d7f5fd4b700b5e5e91cbe
ken0-1n <ubuntu@ip-172-31-34-116.ap-northeast-1.compute.internal>
2020-06-15 05:09:00 +0000

    caluculated the exact junction position

--- bateira_sv/curation_sv.py
+++ bateira_sv/curation_sv.py
@@ -4,7 +4,10 @@ import pysam
 import edlib
 from . import utils
 
-def curation1(input_file, in_bam1, in_bam2, output_file, margin, reference_genome, max_depth, validate_sequence_length, ed_threas = 0.1):
+def curation1(input_file, in_bam1, in_bam2, output_file, margin, \
+            reference_genome, max_depth, \
+            validate_sequence_length, validate_sequence_minus_length, \
+            ed_threas):
 
     bamfile1 = pysam.Samfile(in_bam1, 'rb')
     bamfile2 = pysam.Samfile(in_bam2, 'rb')
@@ -18,33 +21,73 @@ def curation1(input_file, in_bam1, in_bam2, output_file, margin, reference_genom
                 continue  # for geomonSV results
             F = line.split('\t')
 
-            juncChr1 = F[0]
-            juncPos1 = F[1]
-            juncDir1 = F[2]
-            juncChr2 = F[3]
-            juncPos2 = F[4]
-            juncDir2 = F[5]
+            juncChr1,juncPos1,juncDir1  = F[0], F[1], F[2]
+            juncChr2,juncPos2,juncDir2  = F[3], F[4], F[5]
             juncSeq = F[6]
-            target_seq1, count_junc1, count_other1, target_seq2, count_junc2, count_other2 = \
-            geted(juncChr1,juncPos1,juncDir1,juncChr2,juncPos2,juncDir2,juncSeq, bamfile1, max_depth, reference_genome, validate_sequence_length, ed_threas, margin)
             
-            dominant1 = 0.0
-            if count_junc1+count_other1 > 0:
-                dominant1 = count_junc1 / (count_junc1+count_other1)
-            dominant2 = 0.0
-            if count_junc2+count_other2 > 0:
-                dominant2 = count_junc2 / (count_junc2+count_other2)
+            f_inversion = False
+            if (juncDir1 == "+" and juncDir2 == "+") or (juncDir1 == "-" and juncDir2 == "-"):
+                f_inversion = True
+            
+            target_seq2 = getTargetSeq(juncChr2, juncPos2, juncDir2, juncSeq, reference_genome, \
+                        validate_sequence_length, validate_sequence_minus_length, f_inversion)
+            target_seq1 = getTargetSeq(juncChr1, juncPos1, juncDir1, juncSeq, reference_genome, \
+                        validate_sequence_length, validate_sequence_minus_length, f_inversion)
+    
+            ## bam1 start ############
 
-            target_seq1_2, count_junc1_2, count_other1_2, target_seq2_2, count_junc2_2, count_other2_2 = \
-            geted(juncChr1,juncPos1,juncDir1,juncChr2,juncPos2,juncDir2,juncSeq, bamfile2, max_depth, reference_genome, validate_sequence_length, ed_threas, margin)
+            ret_code = checkBamDepth(bamfile1, juncChr1, juncPos1, juncDir1, juncChr2, juncPos2, juncDir2, max_depth)
+            
+            dataframe1 = getTargetDataFrame(bamfile1, juncChr1, juncPos1, juncDir1, \
+                target_seq2, margin, validate_sequence_length, validate_sequence_minus_length, juncSeq)
+    
+            print("----------")
+            
+            h_group = {}
+            for df in dataframe1:
+                # print(df)
+                if len(df["soctclipseq"]) < 10: continue
+                if df["exactposition"] in h_group:
+                    h_group[df["exactposition"]] += 1
+                else:
+                    h_group[df["exactposition"]] = 1
+            
+            print(h_group)        
 
-            print(line +"\t"+target_seq1+"\t"+target_seq2+"\t"+str(round(dominant1,4))+"\t"+str(round(dominant2,4))+"\t"+str(count_junc1_2) +"\t"+ str(count_junc2_2),file=hout)
+    
+            dataframe2 = getTargetDataFrame(bamfile1, juncChr2, juncPos2, juncDir2, \
+                target_seq1, margin, validate_sequence_length, validate_sequence_minus_length, juncSeq)
+            
+            ## bam1 end ############
+            
+            # target_seq1_2, count_junc1_2, count_other1_2, target_seq2_2, count_junc2_2, count_other2_2 = \
+            # geted(juncChr1,juncPos1,juncDir1,juncChr2,juncPos2,juncDir2,juncSeq, bamfile2, max_depth, reference_genome, validate_sequence_length, ed_threas, margin)
+            # print(line +"\t"+target_seq1+"\t"+target_seq2+"\t"+str(round(dominant1,4))+"\t"+str(round(dominant2,4)),file=hout)
+            # print(line +"\t"+target_seq1+"\t"+target_seq2+"\t"+str(round(dominant1,4))+"\t"+str(round(dominant2,4))+"\t"+str(count_junc1_2) +"\t"+ str(count_junc2_2),file=hout)
           
     hout.close  
             
+def getTargetSeq(juncChr, juncPos, targetDir, juncSeq, reference_genome, validate_sequence_length, validate_sequence_minus_length, f_inversion):
 
-def geted(juncChr1,juncPos1,juncDir1,juncChr2,juncPos2,juncDir2,juncSeq, bamfile, max_depth, reference_genome, validate_sequence_length, ed_threas, margin):
+    if juncSeq == "---": juncSeq = ""
     
+    if targetDir == "+":
+        seq = utils.get_seq(reference_genome, juncChr, int(juncPos) - validate_sequence_length, int(juncPos) ) 
+        seq = seq + juncSeq
+        seq = seq + utils.get_seq(reference_genome, juncChr, int(juncPos) + 1, int(juncPos) + validate_sequence_minus_length) 
+    else:
+        seq = utils.get_seq(reference_genome, juncChr, int(juncPos) - validate_sequence_minus_length, int(juncPos) -1 ) 
+        seq = seq + juncSeq
+        seq = seq + utils.get_seq(reference_genome, juncChr, int(juncPos), int(juncPos) + validate_sequence_length) 
+
+    if f_inversion == True:
+        seq = utils.reverseComplement(seq)
+
+    return seq
+
+
+def checkBamDepth(bamfile, juncChr1, juncPos1, juncDir1, juncChr2, juncPos2, juncDir2, max_depth):
+
     # if the #sequence read is over the `maxDepth`, then that key is ignored
     depthFlag = 0
     if bamfile.count(juncChr1, int(juncPos1) - 1, int(juncPos1) + 1) >= max_depth: depthFlag = 1
@@ -53,45 +96,12 @@ def geted(juncChr1,juncPos1,juncDir1,juncChr2,juncPos2,juncDir2,juncSeq, bamfile
         print("sequence depth exceeds the threshould for: " + ','.join([juncChr1, juncPos1, juncDir1, juncChr2, juncPos2, juncDir2]), file = sys.stderr)
         return 1 
 
-    f_inversion = False
-    if (juncDir1 == "+" and juncDir2 == "+") or (juncDir1 == "-" and juncDir2 == "-"):
-        f_inversion = True
 
-    target_seq2 = getTargetSeq(juncChr2, juncPos2, juncDir2, juncSeq, reference_genome, validate_sequence_length, f_inversion)
-    target_seq1 = getTargetSeq(juncChr1, juncPos1, juncDir1, juncSeq, reference_genome, validate_sequence_length, f_inversion)
-                
-    softclipping1, count_junc1, count_other1 = \
-    getTargetSoftclipAndDominant(bamfile, juncChr1, juncPos1, juncDir1, target_seq2, ed_threas, margin)
-    
-    softclipping2, count_junc2, count_other2 = \
-    getTargetSoftclipAndDominant(bamfile, juncChr2, juncPos2, juncDir2, target_seq1, ed_threas, margin)
-    
-    return([softclipping1, count_junc1, count_other1, softclipping2, count_junc2, count_other2]) 
-
-
-def getTargetSeq(juncChr, juncPos, targetDir, juncSeq, reference_genome, validate_sequence_length, f_inversion):
+def getTargetDataFrame(bamfile, juncChr, juncPos, juncDir, target_seq, mergin, validate_sequence_length, validate_sequence_minus_length, juncSeq):
 
     if juncSeq == "---": juncSeq = ""
     
-    if targetDir == "+":
-        seq = utils.get_seq(reference_genome, juncChr, int(juncPos) - validate_sequence_length, int(juncPos) ) 
-        seq = seq + juncSeq
-    else:
-        seq = utils.get_seq(reference_genome, juncChr, int(juncPos), int(juncPos) + validate_sequence_length) 
-        seq = juncSeq + seq
-
-    if f_inversion == True:
-        seq = utils.reverseComplement(seq)
-
-    return seq
-    
-
-def getTargetSoftclipAndDominant(bamfile, juncChr, juncPos, juncDir, target_seq, ed_threas, mergin):
-    
-    ret_seq = ""
-    ret_ed = 100
-    count_other = 0     
-    count_junc = 0   
+    target_dataframe = []
     for read in bamfile.fetch(juncChr, max(0, int(juncPos) - mergin), int(juncPos) + mergin):
 
         # get the flag information
@@ -106,6 +116,7 @@ def getTargetSoftclipAndDominant(bamfile, juncChr, juncPos, juncDir, target_seq,
         # skip duplicated reads
         if flags[10] == "1": continue
     
+        # softclip sequence 
         # M	BAM_CMATCH	0
         # I BAM_CINS	1
         # D	BAM_CDEL	2
@@ -116,43 +127,72 @@ def getTargetSoftclipAndDominant(bamfile, juncChr, juncPos, juncDir, target_seq,
         cigar_target = cigars[-1] if juncDir == "+" else cigars[0]
         clipped_size = cigar_target[1] if int(cigar_target[0]) == 4 else 0 
         
-        if clipped_size < 10: continue
-        
-        # TODO
-        # is the softclipping breakpoint within 10 bp of the junction?
-        tmp_seq = ""
+        if clipped_size == 0: continue
+
+        softclipseq = ""
         if juncDir == "+":
-            tmp_seq = read.seq[-(clipped_size):]
+            softclipseq = read.seq[-(clipped_size):]
         else:
-            tmp_seq = read.seq[:clipped_size]
-        seq = ""
-        # if flags[4] == "1":
-        #     seq = utils.reverseComplement(str(tmp_seq))
-        # else:
-        seq = tmp_seq
+            softclipseq = read.seq[:clipped_size]
 
-        ret = edlib.align(seq, target_seq, mode="HW", task="path")
+        # softcclip pogision
+        softclip_bp_pos = 0
+        if juncDir == "+":
+            softclip_bp_pos = read.aend
+        else:
+            softclip_bp_pos = read.pos + 1
+
+        # edlib
+        ret = edlib.align(softclipseq, target_seq, mode="HW", task="path")
         ed = ret["editDistance"]
+        ed_locations = ret["locations"]
 
-        if (ed/clipped_size) <= ed_threas:
-            if len(seq) > len(ret_seq) or (len(seq) == len(ret_seq) and (ed/clipped_size) < ret_ed):
-                ret_seq = seq
-                ret_ed = (ed/clipped_size)
-                
-        # print (cigars)
-        # print (clipped_size)
-        # print (seq)
-        # print (target_seq2)
-        # print (ed)
-        if (ed/clipped_size) <= ed_threas:
-            count_junc += 1
+        # target alignment position
+        alignment_start_pos = ""
+        alignment_end_pos = ""
+        number_of_bases_matched = -1000
+        multi_blocks = False 
+        # if len(ed_locations) == 1:
+        ed_idx = -1
+        ed_match = -1
+        for index, locate in enumerate(ed_locations):
+            alignment_match = ed_locations[index][1] - ed_locations[index][0]
+            if alignment_match > ed_match:
+                ed_idx = index
+                ed_match = alignment_match
+            
+        if ((ed_locations[ed_idx][1] - ed_locations[ed_idx][0]) / len(softclipseq)) > 0.8:
+            if juncDir == "+":
+                alignment_start_pos = ed_locations[ed_idx][0] - validate_sequence_minus_length
+                alignment_end_pos = ed_locations[ed_idx][1] - validate_sequence_minus_length
+            else:
+                alignment_start_pos = (ed_locations[ed_idx][1] - validate_sequence_length)*-1
+                alignment_end_pos = (ed_locations[ed_idx][0] - validate_sequence_length)*-1
+            number_of_bases_matched = alignment_end_pos - alignment_start_pos
         else:
-            count_other += 1
+            multi_blocks = True
+    
+        # exact position        
+        exact_position = -1000
+        if multi_blocks == False:
+            if juncDir == "+":
+                exact_position = int(alignment_start_pos) + (int(juncPos) - softclip_bp_pos)
+            else:
+                exact_position = int(alignment_start_pos) + len(juncSeq) - (int(juncPos) - softclip_bp_pos)
             
+        h_ret = {"readid":read.qname, "soctclipseq":softclipseq, "direction":juncDir, "juncseq":juncSeq, \
+        "junctionpos":juncPos, "softclipstart": softclip_bp_pos, "multiblocks": multi_blocks, \
+        "location": (alignment_start_pos, alignment_end_pos), "cigars": cigars, "locations":ed_locations, \
+        "basematch": number_of_bases_matched, "editdistance": ed, "exactposition": exact_position}
+        
+        target_dataframe.append(h_ret)
             
-    return([ret_seq, count_junc, count_other]) 
+    return target_dataframe
 
 
 def curation_main(args):
 
-    curation1(args.in_sv, args.in_bam1, args.in_bam2, args.output, args.margin, args.ref_genome, args.max_depth, args.validate_sequence_length)
+    curation1(args.in_sv, args.in_bam1, args.in_bam2, args.output, args.margin, \
+            args.ref_genome, args.max_depth, \
+            args.validate_sequence_length, args.validate_sequence_minus_length,
+            args.ed_threashold)
